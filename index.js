6a22f3acae14acb655d778e18e1abedd223f2194
Kauan-Peterson <kauanpetterson08@gmail.com>
2025-11-18 19:09:28 +0000

    java script extremamente levado

---
+++ index.js
@@ -0,0 +1,9 @@
+function login() {
+    const email = document.getElementById('email').value
+    const senha = document.getElementById('senha').value
+    const botão = document.getElementById('botão').value
+    botão.addEventListener = (alert('sacodorabelo'))
+}
+if (email === email && senha === senha) {
+    
+}
